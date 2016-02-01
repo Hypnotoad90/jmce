227c3e4d463f7407a6bd1fa5a051df1b0200d020
Sean Baggaley <sean@plussean.co.uk>
2016-02-01 20:07:02 +0000

    PK3 versioning

--- build_assets.bat
+++ build_assets.bat
@@ -1,9 +1,10 @@
 @ECHO OFF
 
 CALL "%CD%\utilities\variables.bat"
+CALL "%CD%\utilities\mkvers.bat"
 
 PUSHD jmce_assets
 IF NOT EXIST "%CD%\acs" MKDIR "%CD%\acs"
 acc "%CD%\source\libretro.acs" "%CD%\acs\libretro.o"
-%SEVENZAEXE% a -tzip "%START%\jmce_assets-dev.pk3" * -r -xr!*.dbs -xr!*.backup1 -xr!*.backup2 -xr!*.backup3 -xr!*.bak -xr!.gitignore
+%SEVENZAEXE% a -tzip "%START%\jmce_assets-%FILEVERSION%.pk3" * -r -xr!*.dbs -xr!*.backup1 -xr!*.backup2 -xr!*.backup3 -xr!*.bak -xr!.gitignore
 POPD
