309cc5196926c169f334842c0d7f26c90e3cce87
Sean Baggaley <sean@plussean.co.uk>
2016-02-01 18:41:01 +0000

    Fix jmce_assets being compiled to it's own folder

--- build_assets.bat
+++ build_assets.bat
@@ -5,5 +5,5 @@ CALL "%CD%\utilities\variables.bat"
 PUSHD jmce_assets
 IF NOT EXIST "%CD%\acs" MKDIR "%CD%\acs"
 acc "%CD%\source\libretro.acs" "%CD%\acs\libretro.o"
-%SEVENZAEXE% a -tzip "%CD%\jmce_assets-dev.pk3" * -r -xr!*.dbs -xr!*.backup1 -xr!*.backup2 -xr!*.backup3 -xr!*.bak -xr!.gitignore
+%SEVENZAEXE% a -tzip "%START%\jmce_assets-dev.pk3" * -r -xr!*.dbs -xr!*.backup1 -xr!*.backup2 -xr!*.backup3 -xr!*.bak -xr!.gitignore
 POPD
