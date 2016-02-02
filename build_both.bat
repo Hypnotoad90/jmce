0f496c4a75aa83b778075e9b84c4275abfb843df
Sean Baggaley <sean@plussean.co.uk>
2016-02-02 07:39:06 +0000

    Update build system

--- build_both.bat
+++ build_both.bat
@@ -1,2 +1,8 @@
-@CALL "%CD%\build_assets.bat"
-@CALL "%CD%\build_maps.bat"
+@ECHO OFF
+
+:: More workarounds...
+CALL "%CD%\utilities\variables.bat"
+CALL "%CD%\utilities\mkvers.bat"
+
+CALL "%CD%\build_assets.bat"
+CALL "%CD%\build_maps.bat"
--- build_maps.bat
+++ build_maps.bat
@@ -3,6 +3,8 @@
 CALL "%CD%\utilities\variables.bat"
 CALL "%CD%\utilities\mkvers.bat"
 
+DEL /F /Q "%OUTDIR%\jmce_maps*.pk3"
+
 PUSHD jmce_maps
-%SEVENZAEXE% a -tzip "%START%\jmce_maps-%FILEVERSION%.pk3" * -r -xr!*.dbs -xr!*.backup1 -xr!*.backup2 -xr!*.backup3 -xr!*.bak
+%SEVENZAEXE% a -tzip "%OUTDIR%\jmce_maps%FILEVERSION%.pk3" * -r -xr!*.dbs -xr!*.backup1 -xr!*.backup2 -xr!*.backup3 -xr!*.bak
 POPD
