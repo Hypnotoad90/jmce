a19585cfcc0f8b763b790ea3f4b8dcb295a7dd29
Sean Baggaley <sean@plussean.co.uk>
2016-02-03 08:07:04 +0000

    Compile libcxj.acs

--- build_assets.bat
+++ build_assets.bat
@@ -8,5 +8,6 @@ DEL /F /Q "%OUTDIR%\jmce_assets*.pk3"
 PUSHD jmce_assets
 IF NOT EXIST "%CD%\acs" MKDIR "%CD%\acs"
 acc "%CD%\source\libretro.acs" "%CD%\acs\libretro.o"
+acc "%CD%\source\libcxj.acs" "%CD%\acs\libcxj.o"
 %SEVENZAEXE% a -tzip "%OUTDIR%\jmce_assets%FILEVERSION%.pk3" * -r -xr!*.dbs -xr!*.backup1 -xr!*.backup2 -xr!*.backup3 -xr!*.bak -xr!.gitignore
 POPD
